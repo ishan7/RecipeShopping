c95958fc284f48df089e9bf190390f5bab95ad23
Ishan Uniyal <life.ishan1@gmail.com>
2020-05-13 18:12:38 +0530

    updated recipie component

--- ShoppingProject/src/app/recipies/recipie-list/recipie-list.component.ts
+++ ShoppingProject/src/app/recipies/recipie-list/recipie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipie } from "../recipie.model";
 
 @Component({
   selector: 'app-recipie-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipie-list.component.css']
 })
 export class RecipieListComponent implements OnInit {
+
+  recipies: Recipie[] = [
+new Recipie('a test recipie', 'description', 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1600%2Cc_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'),
+new Recipie('a test recipie', 'description', 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1600%2Cc_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'),new Recipie('a test recipie', 'description', 'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/16:9/w_1600%2Cc_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg')
+
+  ];
 
   constructor() { }
 
