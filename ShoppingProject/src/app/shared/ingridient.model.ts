f46d37eefcb0bfde6cb63a72c7d8537fcb747c83
Ishan Uniyal <life.ishan1@gmail.com>
2020-05-14 09:25:52 +0530

    shopping module UI

---
+++ ShoppingProject/src/app/shared/ingridient.model.ts
@@ -0,0 +1,13 @@
+export class Ingridient {
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name:string,amount:number)
+    // {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+constructor(public name:string, public amount: number)
+{}
+
+}
